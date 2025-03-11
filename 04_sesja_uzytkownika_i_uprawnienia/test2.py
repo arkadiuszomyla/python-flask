467001fcf56411a9cad43a6a464a8112b37b0e3b
Arkadiusz Omya <arkadiusz.omyla@codearm.pl>
2025-03-11 10:03:34 +0100

    - test laptop_s

---
+++ 04_sesja_uzytkownika_i_uprawnienia/test2.py
@@ -0,0 +1,4 @@
+git checkout master  # Przełącz na master
+git pull origin master  # Pobierz najnowsze zmiany z mastera (opcjonalnie)
+git merge laptop_s  # Scal laptop_s do mastera
+git push origin master  # Wypchnij zmiany na GitHub
